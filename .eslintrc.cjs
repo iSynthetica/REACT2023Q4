7de1aef5c3a28daac37517544fbd2366607ca890
iSynthetica <syntheticafreon@gmail.com>
2023-10-25 23:10:45 +0300

    dev: set up husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,27 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, jest: true },
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true
+  },
   extends: [
     'plugin:@typescript-eslint/recommended',
     'prettier',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+    'vite.config.ts'
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks'
+  ],
   rules: {
-    'prettier/prettier': 'error',
-    '@typescript-eslint/no-explicit-any': 'error',
-  },
+    '@typescript-eslint/no-explicit-any': 'error'
+  }
 };
