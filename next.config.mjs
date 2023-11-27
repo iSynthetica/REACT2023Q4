2f7d837b044cc1fbbf83bf1559ba63583aed567f
iSynthetica <syntheticafreon@gmail.com>
2023-11-27 22:36:49 +0200

    dev: remove base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: './dist', // Changes the build output directory to `./dist/`.
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
     remotePatterns: [
       {
